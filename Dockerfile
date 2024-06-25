7b7c47f06690dad437165009342172fc63ee39cf
Vladimir Alekseev <vcs@avs.guru>
2024-06-25 19:29:37 +0300

    first version

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# Используем официальный образ Node.js в качестве базового образа
+FROM node:18-alpine AS builder
+
+# Создаем временную рабочую директорию для сборки Strapi-приложения
+WORKDIR /tmp
+
+# Устанавливаем необходимые зависимости и создаем Strapi-приложение
+RUN yarn create strapi-app strapi --dbclient sqlite --skip-cloud --ts
+
+# Переходим к финальному образу
+FROM node:18-alpine
+
+# Установка rsync
+RUN apk add --no-cache rsync
+
+# Создаем рабочую директорию для приложения
+WORKDIR /usr/src/strapi
+
+# Копируем созданное Strapi-приложение из временного образа
+COPY --from=builder /tmp/strapi .
+#
+## Устанавливаем зависимости
+#RUN yarn install
+#
+#RUN yarn build
+
+# Открываем порт 1337, который используется Strapi
+EXPOSE 1337
+
+# Команда для запуска Strapi
+CMD ["yarn", "develop"]
---
+++ init.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Остановить скрипт при ошибке
+set -e
+
+# Функция для первичной сборки
+initial_build() {
+  echo "Running initial build..."
+  docker-compose -f docker-compose.build.yml build
+  docker-compose -f docker-compose.build.yml up -d
+
+  container_id=$(docker-compose -f docker-compose.build.yml ps -q strapi)
+  echo "Container ID: $container_id"
+
+  # Подождите немного, чтобы контейнер успел запуститься
+  sleep 10
+
+  # Копируйте файлы из контейнера в локальную директорию
+  echo "Copying files from container to host..."
+  docker cp $container_id:/usr/src/strapi ./app
+
+  # Остановите и удалите контейнеры после копирования
+  docker-compose -f docker-compose.build.yml down
+}
+
+# Функция для запуска контейнера с монтированием
+run_with_volume() {
+  echo "Running with volume mounted..."
+  docker-compose up -d
+}
+
+# Проверьте, существует ли локальная директория ./app
+if [ ! -d "./app" ]; then
+  initial_build
+fi
+
+# Запуск контейнера с монтированием
+run_with_volume
+
+# Вывести статус контейнеров
+docker-compose ps
+
+echo "Setup complete."
