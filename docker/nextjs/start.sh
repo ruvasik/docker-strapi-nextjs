8e3b7ee5a1df1a0394982ef0227ee3fe45a928d0
Vladimir Alekseev <vcs@avs.guru>
2024-06-29 01:17:51 +0300

    docker-compose tpls with env

--- docker/nextjs/start.sh
+++ docker/nextjs/start.sh
@@ -3,9 +3,12 @@
 # Stop on error
 set -e
 
+export $(grep -v '^#' .env | xargs)
+
 # Initialize env variables
 SH_MODE=false
 YARN_MODE=false
+SERVICE="${APP_NAME}-nextjs"
 
 # Set default values for env variables
 for arg in "$@"
@@ -24,10 +27,13 @@ done
 # First run build
 initial_build() {
   echo "Running initial build..."
+
+  envsubst < docker/nextjs/docker-compose.build.tpl.yml > docker/nextjs/docker-compose.build.yml
+
   docker-compose --env-file .env -f docker/nextjs/docker-compose.build.yml -f docker/docker-compose.networks.yml build
   docker-compose --env-file .env -f docker/nextjs/docker-compose.build.yml -f docker/docker-compose.networks.yml up -d
 
-  container_id=$(docker-compose -f docker/nextjs/docker-compose.build.yml ps -q next-app)
+  container_id=$(docker-compose -f docker/nextjs/docker-compose.build.yml ps -q "${SERVICE}")
   echo "Container ID: $container_id"
 
   sleep 5
@@ -36,6 +42,9 @@ initial_build() {
   echo "Copying files from container to host..."
   docker cp $container_id:/usr/app ./frontend
 }
+
+envsubst < docker/docker-compose.networks.tpl.yml > docker/docker-compose.networks.yml
+envsubst < docker/nextjs/docker-compose.tpl.yml > docker/nextjs/docker-compose.yml
 
 # Check /frontend, if not - run initial build
 if [ ! -d "./frontend" ]; then
@@ -46,16 +55,15 @@ if [ -z "$arg" ]; then
   echo "FRONTEND: yarn dev mode"
 
   docker-compose --env-file .env -f docker/nextjs/docker-compose.yml -f docker/docker-compose.networks.yml up -d
-#  docker-compose -f docker/nextjs/docker-compose.yml exec next-app /bin/sh -c "yarn dev"
   docker-compose --env-file .env -f docker/nextjs/docker-compose.yml -f docker/docker-compose.networks.yml logs
 elif [ "$SH_MODE" = true ]; then
   echo "FRONTEND: sh mode";
 
   docker-compose --env-file .env -f docker/nextjs/docker-compose.yml -f docker/docker-compose.networks.yml up -d
-  docker-compose --env-file .env -f docker/nextjs/docker-compose.yml -f docker/docker-compose.networks.yml exec next-app /bin/sh
+  docker-compose --env-file .env -f docker/nextjs/docker-compose.yml -f docker/docker-compose.networks.yml exec "${SERVICE}" /bin/sh
 else
   echo "FRONTEND: yarn mode with arg: $arg";
 
   docker-compose --env-file .env -f docker/nextjs/docker-compose.yml -f docker/docker-compose.networks.yml up -d
-  docker-compose --env-file .env -f docker/nextjs/docker-compose.yml -f docker/docker-compose.networks.yml exec next-app /bin/sh -c "yarn $arg"
+  docker-compose --env-file .env -f docker/nextjs/docker-compose.yml -f docker/docker-compose.networks.yml exec "${SERVICE}" /bin/sh /bin/sh -c "yarn $arg"
 fi
--- docker/strapi/start.sh
+++ docker/strapi/start.sh
@@ -3,13 +3,20 @@
 # Остановить скрипт при ошибке
 set -e
 
+export $(grep -v '^#' .env | xargs)
+
+SERVICE="${APP_NAME}-strapi"
+
 # Функция для первичной сборки
 initial_build() {
   echo "BACKEND: Running initial build..."
+
+  envsubst < docker/strapi/docker-compose.build.tpl.yml > docker/strapi/docker-compose.build.yml
+
   docker-compose --env-file .env -f docker/strapi/docker-compose.yml -f docker/docker-compose.networks.yml build
   docker-compose --env-file .env -f docker/strapi/docker-compose.yml -f docker/docker-compose.networks.yml up -d
 
-  container_id=$(docker-compose --env-file .env -f docker/strapi/docker-compose.build.yml ps -q strapi)
+  container_id=$(docker-compose --env-file .env -f docker/strapi/docker-compose.build.yml ps -q "${SERVICE}")
   echo "BACKEND: Container ID: $container_id"
 
   # Подождите немного, чтобы контейнер успел запуститься
@@ -22,6 +29,9 @@ initial_build() {
   # Остановите и удалите контейнеры после копирования
 #  docker-compose -f docker/strapi/docker-compose.build.yml down
 }
+
+envsubst < docker/docker-compose.networks.tpl.yml > docker/docker-compose.networks.yml
+envsubst < docker/strapi/docker-compose.tpl.yml > docker/strapi/docker-compose.yml
 
 # Функция для запуска контейнера с монтированием
 run_with_volume() {
